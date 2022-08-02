ec0c7c8108a0e7473d9eb6a38b0cfff2a15faddf
DeltaZero <233a344a455@gmail.com>
2022-08-02 09:17:59 +0800

    Use base64 filename encoder instead of base85

--- app/src/main/java/deltazero/amarok/FileEncoder.java
+++ app/src/main/java/deltazero/amarok/FileEncoder.java
@@ -1,10 +1,11 @@
 package deltazero.amarok;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import android.util.Base64;
 import android.util.Log;
 
 import androidx.documentfile.provider.DocumentFile;
-
-import java.nio.charset.StandardCharsets;
 
 public class FileEncoder {
     private final static String TAG = "FileProc";
@@ -15,8 +16,7 @@ public class FileEncoder {
 
         // Check if the file have already been encoded.
         try {
-            if ((new String(Base85.getRfc1924Decoder().decodeToBytes(f.getName()), StandardCharsets.UTF_8))
-                    .startsWith(ENCODED_TAG)) {
+            if (new String(Base64.decode(f.getName(), Base64.URL_SAFE | Base64.NO_WRAP | Base64.NO_PADDING), UTF_8).startsWith(ENCODED_TAG)) {
                 Log.d(TAG, "Found encoded filename: " + f.getUri().getLastPathSegment() + ", skip...");
                 return;
             }
@@ -24,17 +24,17 @@ public class FileEncoder {
             // Decode attempt failed, process encoding
         }
 
-        String encodedName = Base85.getRfc1924Encoder().encodeToString((ENCODED_TAG + f.getName()).getBytes(StandardCharsets.UTF_8));
+        String encodedName = Base64.encodeToString((ENCODED_TAG + f.getName()).getBytes(UTF_8), Base64.URL_SAFE | Base64.NO_WRAP | Base64.NO_PADDING);
 
         Log.d(TAG, "Encode: " + f.getUri().getLastPathSegment() + " -> " + encodedName);
-        f.renameTo(encodedName);
+//        f.renameTo(encodedName);
 
     }
 
     private static void decode(DocumentFile f) {
         assert f.getName() != null;
         try {
-            String decodedName = new String(Base85.getRfc1924Decoder().decodeToBytes(f.getName()), StandardCharsets.UTF_8);
+            String decodedName = new String(Base64.decode(f.getName(), Base64.URL_SAFE | Base64.NO_WRAP | Base64.NO_PADDING), UTF_8);
             if (!decodedName.startsWith(ENCODED_TAG)) {
                 Log.w(TAG, "Found plain filename: " + f.getUri().getLastPathSegment() + ", skip...");
                 return;
@@ -42,7 +42,7 @@ public class FileEncoder {
             decodedName = decodedName.replace(ENCODED_TAG, "");
 
             Log.d(TAG, "Decode: " + f.getUri().getLastPathSegment() + " -> " + decodedName);
-            f.renameTo(decodedName);
+//            f.renameTo(decodedName);
 
         } catch (IllegalArgumentException e) {
             Log.w(TAG, "Unable to decode: " + f.getUri().getLastPathSegment());
