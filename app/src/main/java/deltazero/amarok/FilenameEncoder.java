9a852ae9f05a731ee752c553bab48d061b4d4403
DeltaZero <233a344a455@gmail.com>
2022-08-02 13:12:47 +0800

    Optimize filename encoding speed

---
+++ app/src/main/java/deltazero/amarok/FilenameEncoder.java
@@ -0,0 +1,95 @@
+package deltazero.amarok;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import android.util.Base64;
+import android.util.Log;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class FilenameEncoder {
+    private final static String TAG = "NameEncoder";
+    private final static String ENCODED_TAG = "[AMAROK]";
+    private final static int BASE64_TAG = Base64.URL_SAFE | Base64.NO_WRAP | Base64.NO_PADDING;
+
+    private static void processFilename(Path path, ProcessMethod processMethod) throws IOException {
+        String filename = path.getFileName().toString();
+        Path newPath = null;
+
+        if (processMethod == ProcessMethod.ENCODE) {
+
+            // Check if the filename have already been encoded.
+            try {
+                if (new String(Base64.decode(filename, BASE64_TAG), UTF_8)
+                        .startsWith(ENCODED_TAG)) {
+                    Log.d(TAG, "Found encoded filename: " + filename + ", skip...");
+                    return;
+                }
+            } catch (IllegalArgumentException e) {
+                // Filename is not encoded
+            }
+
+            filename = Base64.encodeToString((ENCODED_TAG + filename).getBytes(UTF_8), BASE64_TAG);
+            newPath = Paths.get(path.getParent().toString(), filename);
+
+            Log.d(TAG, "Encode: " + path.toString() + " -> " + newPath.toString());
+
+        } else if (processMethod == ProcessMethod.DECODE) {
+
+            try {
+                filename = new String(Base64.decode(filename, BASE64_TAG), UTF_8);
+                if (!filename.startsWith(ENCODED_TAG)) {
+                    Log.w(TAG, "Found not coded text: " + path.getFileName().toString() + ", skip...");
+                    return;
+                }
+                newPath = Paths.get(path.getParent().toString(), filename.replace(ENCODED_TAG, ""));
+                Log.d(TAG, "Decode: " + path.toString() + " -> " + newPath.toString());
+            } catch (IllegalArgumentException e) {
+                Log.w(TAG, "Unable to decode: " + filename);
+                return;
+            }
+
+        }
+
+        Files.move(path, newPath);
+
+    }
+
+    public static void process(Path targetDir, ProcessMethod processMethod) {
+        Log.d(TAG, "Applying hider to: " + targetDir);
+
+        try {
+            Files.walkFileTree(targetDir, new SimpleFileVisitor<Path>() {
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                        throws IOException {
+                    processFilename(file, processMethod);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException e)
+                        throws IOException {
+                    if (dir != targetDir)
+                        processFilename(dir, processMethod);
+                    return FileVisitResult.CONTINUE;
+                }
+
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public enum ProcessMethod {
+        ENCODE, DECODE
+    }
+}
--- app/src/main/java/deltazero/amarok/Utils.java
+++ app/src/main/java/deltazero/amarok/Utils.java
@@ -1,11 +1,55 @@
 package deltazero.amarok;
 
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.os.Build.VERSION.SDK_INT;
+
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.Settings;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 //import com.catchingnow.icebox.sdk_client.IceBox;
 
 public class Utils {
+//
+//    private static int PERMISSION_REQUEST_CODE = 100;
+//
+//    private boolean checkStoragePermission(Context context) {
+//        if (SDK_INT >= Build.VERSION_CODES.R) {
+//            // For android 11+
+//            return Environment.isExternalStorageManager();
+//        } else {
+//            int read = ContextCompat.checkSelfPermission(context, READ_EXTERNAL_STORAGE);
+//            int write = ContextCompat.checkSelfPermission(context, WRITE_EXTERNAL_STORAGE);
+//            return read == PackageManager.PERMISSION_GRANTED && write == PackageManager.PERMISSION_GRANTED;
+//        }
+//    }
+//
+//    private void requestStoragePermission(Context context) {
+//        if (SDK_INT >= Build.VERSION_CODES.R) {
+//            try {
+//                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+//                intent.addCategory("android.intent.category.DEFAULT");
+//                intent.setData(Uri.parse(String.format("package:%s",getApplicationContext().getPackageName())));
+//                startActivityForResult(intent, 2296);
+//            } catch (Exception e) {
+//                Intent intent = new Intent();
+//                intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+//                startActivityForResult(intent, 2296);
+//            }
+//        } else {
+//            //below android 11
+//            ActivityCompat.requestPermissions(context, new String[]{WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+//        }
+//    }
+
 //    static public IceBox.SilentInstallSupport getIceboxAvailability(Context context) {
 //        try {
 //            context.getPackageManager().getPackageInfo("com.catchingnow.icebox", 0);
