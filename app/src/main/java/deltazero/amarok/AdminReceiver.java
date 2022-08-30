c2594693baf2075d4227a106b2ea5cef7ab26033
deltazefiro <deltazefiro@gmail.com>
2022-08-31 07:38:13 +0800

    Init DSM mode

---
+++ app/src/main/java/deltazero/amarok/AdminReceiver.java
@@ -0,0 +1,15 @@
+package deltazero.amarok;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+
+public class AdminReceiver extends DeviceAdminReceiver {
+
+    @Override
+    public void onEnabled(@NonNull Context context, @NonNull Intent intent) {
+        super.onEnabled(context, intent);
+    }
+}
