261a849a518456c9d4a780fb183426aab4be7780
deltazefiro <deltazefiro@gmail.com>
2022-08-31 10:52:28 +0800

    Optimize app list layout

---
+++ app/src/main/java/deltazero/amarok/ui/SettingsActivity.java
@@ -0,0 +1,16 @@
+package deltazero.amarok.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import deltazero.amarok.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
