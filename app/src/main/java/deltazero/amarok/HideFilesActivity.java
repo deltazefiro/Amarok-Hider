77f3961e4c40830339f512177add0718931cf4e5
deltazefiro <deltazefiro@gmail.com>
2022-09-11 08:36:19 +0800

    Fix MainActivity status bar color in dark mode

---
+++ app/src/main/java/deltazero/amarok/HideFilesActivity.java
@@ -0,0 +1,14 @@
+package deltazero.amarok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HideFilesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hidefiles);
+    }
+}
