199f156da3e7d7346691723ca7a577931adc4e69
deltazefiro <deltazefiro@gmail.com>
2022-10-17 19:46:43 +0800

    Change docs link

--- app/src/main/java/deltazero/amarok/ui/SettingsActivity.java
+++ app/src/main/java/deltazero/amarok/ui/SettingsActivity.java
@@ -164,7 +164,7 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     public void openHelp(View view){
-        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://deltazefiro.github.io/Amarok-doc/faq.html")));
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://deltazefiro.github.io/Amarok-doc/")));
 
     }
 
