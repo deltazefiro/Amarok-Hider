d40649b446959cbaa33cc130b7be042b4f09b963
deltazefiro <deltazefiro@gmail.com>
2022-12-26 13:04:39 +0800

    style: Code clean up

--- app/src/main/java/deltazero/amarok/utils/PermissionUtil.java
+++ app/src/main/java/deltazero/amarok/utils/PermissionUtil.java
@@ -1,7 +1,6 @@
 package deltazero.amarok.utils;
 
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -11,11 +10,7 @@ import com.hjq.permissions.XXPermissions;
 
 import java.util.List;
 
-import deltazero.amarok.AppHider.ShizukuHider;
-import deltazero.amarok.PrefMgr;
 import deltazero.amarok.R;
-import deltazero.amarok.ui.SettingsActivity;
-import rikka.shizuku.Shizuku;
 
 
 public class PermissionUtil {
@@ -51,44 +46,4 @@ public class PermissionUtil {
 
     }
 
-//
-//    private static int PERMISSION_REQUEST_CODE = 100;
-//
-//    private boolean checkStoragePermission(Context context) {
-//        if (SDK_INT >= Build.VERSION_CODES.R) {
-//            // For android 11+
-//            return Environment.isExternalStorageManager();
-//        } else {
-//            int read = ContextCompat.checkSelfPermission(context, READ_EXTERNAL_STORAGE);
-//            int write = ContextCompat.checkSelfPermission(context, WRITE_EXTERNAL_STORAGE);
-//            return read == PackageManager.PERMISSION_GRANTED && write == PackageManager.PERMISSION_GRANTED;
-//        }
-//    }
-//
-//    private void requestStoragePermission(Context context) {
-//        if (SDK_INT >= Build.VERSION_CODES.R) {
-//            try {
-//                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
-//                intent.addCategory("android.intent.category.DEFAULT");
-//                intent.setData(Uri.parse(String.format("package:%s",getApplicationContext().getPackageName())));
-//                startActivityForResult(intent, 2296);
-//            } catch (Exception e) {
-//                Intent intent = new Intent();
-//                intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
-//                startActivityForResult(intent, 2296);
-//            }
-//        } else {
-//            //below android 11
-//            ActivityCompat.requestPermissions(context, new String[]{WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
-//        }
-//    }
-
-//    static public IceBox.SilentInstallSupport getIceboxAvailability(Context context) {
-//        try {
-//            context.getPackageManager().getPackageInfo("com.catchingnow.icebox", 0);
-//            return IceBox.querySupportSilentInstall(context);
-//        } catch (PackageManager.NameNotFoundException e) {
-//            return IceBox.SilentInstallSupport.NOT_INSTALLED;
-//        }
-//    }
 }
