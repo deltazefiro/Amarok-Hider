5d0fbb44d1074315b6b443e81ed41389624bb7ca
deltazefiro <deltazefiro@gmail.com>
2022-12-27 19:18:26 +0800

    feat: Add SwitchAppHider activity

---
+++ app/src/main/java/deltazero/amarok/ui/SwitchFileHiderActivity.java
@@ -0,0 +1,63 @@
+package deltazero.amarok.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.materialswitch.MaterialSwitch;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
+import deltazero.amarok.PrefMgr;
+import deltazero.amarok.R;
+
+public class SwitchFileHiderActivity extends AppCompatActivity {
+
+    private PrefMgr prefMgr;
+    private MaterialSwitch swEnableCorruptFileHider;
+    private MaterialToolbar tbToolBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_switch_filehider);
+
+        prefMgr = new PrefMgr(this);
+
+        swEnableCorruptFileHider = findViewById(R.id.switch_filehider_sw_corrupt_header);
+        tbToolBar = findViewById(R.id.switch_filehider_tb_toolbar);
+
+        swEnableCorruptFileHider.setChecked(prefMgr.getEnableCorruptFileHeader());
+        swEnableCorruptFileHider.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            prefMgr.setEnableCorruptFileHeader(isChecked);
+        });
+
+        // Enable back button
+        tbToolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        // Disable switches if hidden.
+        if (prefMgr.getIsHidden()) {
+            swEnableCorruptFileHider.setEnabled(false);
+            Snackbar.make(tbToolBar, R.string.option_unava_when_hidden, Snackbar.LENGTH_INDEFINITE).show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (prefMgr.getIsHidden()) {
+            swEnableCorruptFileHider.setEnabled(false);
+            Snackbar.make(tbToolBar, R.string.option_unava_when_hidden, Snackbar.LENGTH_INDEFINITE).show();
+        }
+    }
+}
