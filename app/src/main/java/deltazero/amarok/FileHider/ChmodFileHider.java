b7baa180651b1285207d4e6a0cf2a24256efc21d
deltazefiro <deltazefiro@gmail.com>
2023-08-22 12:57:37 +0800

    feat: Add ChmodFileHider

---
+++ app/src/main/java/deltazero/amarok/FileHider/ChmodFileHider.java
@@ -0,0 +1,33 @@
+package deltazero.amarok.FileHider;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import deltazero.amarok.utils.ShellUtil;
+
+public class ChmodFileHider extends FileHiderBase {
+
+    public ChmodFileHider(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void process(Set<String> targetDirs, ProcessMethod method) throws InterruptedException {
+
+        var processDirs = new HashSet<String>();
+        for (var d : targetDirs) {
+            if (d.startsWith("/storage/emulated/0/"))
+                processDirs.add(d.replace("/storage/emulated/", "/data/media/"));
+            else
+                Log.w("ChmodFileHider", String.format("Unsupported path: %s", d));
+        }
+
+        StringBuilder cmd = new StringBuilder("echo amarok");
+        for (String d : processDirs)
+            cmd.append(String.format("&chmod -R %s %s", method == ProcessMethod.HIDE ? 0 : 2770, d));
+        ShellUtil.exec(new String[]{"su", "-c", cmd.toString()});
+    }
+}
