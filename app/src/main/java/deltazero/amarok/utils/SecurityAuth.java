3536df401e025ddec32d69fcac5b70f74b1a7230
deltazefiro <deltazefiro@gmail.com>
2023-12-10 12:33:22 +0800

    feat: Prevent start Activity from QSTile when the SecurityAuth has passed

--- app/src/main/java/deltazero/amarok/utils/SecurityAuth.java
+++ app/src/main/java/deltazero/amarok/utils/SecurityAuth.java
@@ -1,8 +1,12 @@
 package deltazero.amarok.utils;
 
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.biometric.BiometricPrompt;
 import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.MutableLiveData;
 
 import deltazero.amarok.PrefMgr;
 import deltazero.amarok.R;
@@ -10,42 +14,61 @@ import deltazero.amarok.ui.PasswordAuthFragment;
 
 public class SecurityAuth {
 
+    public static boolean locked = true;
+    public static SecurityAuth instance;
+
     public interface SecurityAuthCallback {
         void onSecurityAuthCallback(boolean succeed);
     }
 
-    private SecurityAuthCallback callback;
-    private FragmentActivity activity;
+    private final SecurityAuthCallback callback;
+    private final FragmentActivity activity;
     private PasswordAuthFragment passwordAuthFragment;
+    private BiometricPrompt biometricPrompt;
 
     public SecurityAuth(FragmentActivity activity, SecurityAuthCallback callback) {
         this.callback = callback;
         this.activity = activity;
-        if (passwordAuthFragment == null)
-            passwordAuthFragment = new PasswordAuthFragment();
+
+        if (instance != null) instance.cancel();
+        instance = this;
     }
 
     public void authenticate() {
-        if (passwordAuthFragment.isAdded())
-            passwordAuthFragment.dismiss();
-        if (PrefMgr.getAmarokPassword() != null) {
-            if (PrefMgr.getEnableAmarokBiometricAuth()) biometricAuthenticate();
-            else passwordAuthenticate();
-        } else {
+
+        if (PrefMgr.getAmarokPassword() == null || !locked) {
             callback.onSecurityAuthCallback(true);
+            return;
         }
+
+        Log.i("SecurityAuth", "Authentication triggered");
+        if (PrefMgr.getEnableAmarokBiometricAuth()) biometricAuthenticate();
+        else passwordAuthenticate();
+    }
+
+    public void cancel() {
+        if (passwordAuthFragment != null && passwordAuthFragment.isAdded())
+            passwordAuthFragment.dismiss();
+        if (biometricPrompt != null)
+            biometricPrompt.cancelAuthentication();
+        callback.onSecurityAuthCallback(false);
     }
 
     private void passwordAuthenticate() {
         assert PrefMgr.getAmarokPassword() != null;
-        passwordAuthFragment
-                .setOnVerifiedCallback(succeed -> callback.onSecurityAuthCallback(succeed))
-                .show(activity.getSupportFragmentManager(), null);
+
+        passwordAuthFragment = new PasswordAuthFragment()
+                .setOnVerifiedCallback(succeed -> {
+                    if (succeed) locked = false;
+                    callback.onSecurityAuthCallback(succeed);
+                });
+
+        passwordAuthFragment.show(activity.getSupportFragmentManager(), null);
     }
 
     private void biometricAuthenticate() {
 
-        var biometricPrompt = new BiometricPrompt(activity, new BiometricPrompt.AuthenticationCallback() {
+        biometricPrompt = new BiometricPrompt(activity, new BiometricPrompt.AuthenticationCallback() {
             @Override
             public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
                 passwordAuthenticate();
@@ -53,6 +76,7 @@ public class SecurityAuth {
 
             @Override
             public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                locked = false;
                 callback.onSecurityAuthCallback(true);
             }
 
