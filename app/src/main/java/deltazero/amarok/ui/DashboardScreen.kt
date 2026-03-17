dd93784055dd8345df2fd0a022ee767a2c2bb10c
deltazefiro <deltazefiro@gmail.com>
2026-03-17 09:08:35 +0800

    chore: Cleanup obsolete files and update docs
    - Remove SettingsActivity, SetHideFilesActivity, SetHideFilesScreen
    - Remove dead layout-land/activity_main.xml
    - Remove activity declarations from AndroidManifest
    - Remove unused imports and dead code
    - Update overview doc for 4-tab architecture

--- app/src/main/java/deltazero/amarok/ui/DashboardScreen.kt
+++ app/src/main/java/deltazero/amarok/ui/DashboardScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.foundation.layout.offset
 import androidx.lifecycle.viewmodel.compose.viewModel
 import deltazero.amarok.core.Hider
