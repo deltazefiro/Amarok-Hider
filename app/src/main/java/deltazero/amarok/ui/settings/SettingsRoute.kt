1eca53197f554f89ecfc3a3d6ecde14218adc464
deltazefiro <deltazefiro@gmail.com>
2026-06-26 11:19:47 +0800

    chore: Bump dependencies

--- app/src/main/java/deltazero/amarok/ui/settings/SettingsRoute.kt
+++ app/src/main/java/deltazero/amarok/ui/settings/SettingsRoute.kt
@@ -6,7 +6,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalContext
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import deltazero.amarok.utils.UpdateUtil
 
 @Composable
